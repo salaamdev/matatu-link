8b803ee824266633d1c53ed2e2b0ce69c357638c
salaamdev <abdisalamxhassan@gmail.com>
2024-10-26 08:27:22 +0300

    integration of db

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
+import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {initDatabase} from './services/database';
+
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 
+import OnboardingScreen from './screens/OnboardingScreen';
+import HomeScreen from './screens/HomeScreen';
+import ContributeScreen from './screens/ContributeScreen';
+import FeedbackScreen from './screens/FeedbackScreen';
+import LiveTrackingScreen from './screens/LiveTrackingScreen';
+import NotificationsScreen from './screens/NotificationsScreen';
+import RouteDetailsScreen from './screens/RouteDetailsScreen';
+import SafetyTipsScreen from './screens/SafetyTipsScreen';
+import SearchResultsScreen from './screens/SearchResultsScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import UserProfileScreen from './screens/UserProfileScreen';
+
+const Stack = createStackNavigator();
+
 export default function App () {
+  useEffect(() => {
+    initDatabase();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Contribute" component={ContributeScreen} />
+        <Stack.Screen name="Feedback" component={FeedbackScreen} />
+        <Stack.Screen name="LiveTracking" component={LiveTrackingScreen} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        <Stack.Screen name="RouteDetails" component={RouteDetailsScreen} />
+        <Stack.Screen name="SafetyTips" component={SafetyTipsScreen} />
+        <Stack.Screen name="SearchResults" component={SearchResultsScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="UserProfile" component={UserProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
