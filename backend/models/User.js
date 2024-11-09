ee3d5cc01a26d15cd764964f7fa919ed60dc68df
SalaamDev <abdisalamxhassan@gmail.com>
2024-11-09 11:55:00 +0300

    Add User model with Sequelize for user data management

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    username: {type: DataTypes.STRING, unique: true, allowNull: false},
+    email: {type: DataTypes.STRING, unique: true, allowNull: false},
+    passwordHash: {type: DataTypes.STRING, allowNull: false},
+    roleId: {type: DataTypes.INTEGER},
+    isActive: {type: DataTypes.BOOLEAN, defaultValue: true},
+}, {timestamps: true});
+
+module.exports = User;
