663ae3f8bfba92e91caf0c2a2b2bb3e9d0189d54
SalaamDev <abdisalamxhassan@gmail.com>
2024-11-09 21:51:40 +0300

    Update db.js: enable logging for database queries in development

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Turn off logging in production
+    logging: console.log, // Turn off logging in production
 });
 
 module.exports = sequelize;
